4ab0823435880f8e75f56a75f8925c73be921acd
Gaby <gabyperez.dev@gmail.com>
2021-12-21 12:09:22 -0700

    Project setup

---
+++ src/RecipeDetail.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const RecipeDetail = () => {
+    return (
+        <div>
+            Recipe detail
+
+            <Link to="/">Home</Link>
+        </div>
+    )
+}
+
+export default RecipeDetail
