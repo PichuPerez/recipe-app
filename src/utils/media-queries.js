b8c8fe711258d6f1bbf1da38b8a4f5ab9133a33e
Gaby <gabyperez.dev@gmail.com>
2021-12-21 15:33:03 -0700

    Added assets and device media query

---
+++ src/utils/media-queries.js
@@ -0,0 +1,6 @@
+export const media = {
+    s: `@media only screen and (max-width: 576px)`,
+    m: `@media only screen and (max-width: 768px)`,
+    l: `@media only screen and (max-width: 992px)`,
+    xl: `@media only screen and (max-width: 1200px)`,
+}
