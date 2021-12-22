a06bc53675cf7792ecadb5219e395136a09f2d21
Gaby <gabyperez.dev@gmail.com>
2021-12-21 22:30:49 -0700

    Styled recipe card

--- src/components/RecipeCard.tsx
+++ src/components/RecipeCard.tsx
@@ -1,10 +1,54 @@
 import React from 'react'
+import { Recipe } from '../models/recipe'
+import styled from 'styled-components'
+import { media } from '../utils/media-queries'
 
-const RecipeCard = () => {
+interface RecipeCardProps {
+    recipe: Recipe
+}
+
+interface ImageProps {
+    imageUrl: string;
+}
+const s = {
+    CardContainer: styled.div`
+        border: 1px solid #80808036;
+        height: 400px;
+        border-radius: 4px;
+        margin: 10px;
+        width: 300px;
+        display: flex;
+        flex-direction: column;
+        ${media.m}{
+            width: 98%;
+        }
+    `,
+    Image: styled.div<ImageProps>`
+        background-image: url(${(props) => props.imageUrl});
+        background-size: cover;
+        background-position: center;
+        width: 100%;
+        height: 250px;  
+        border-radius: 4px 4px 0px 0px;
+    `,
+    Title: styled.h2`
+        margin-left: 15px;
+        margin-right: 15px;
+    `,
+    Text: styled.p`
+        margin: 0px;
+        margin-left: 15px;
+
+    `
+}
+const RecipeCard = ({ recipe }: RecipeCardProps) => {
     return (
-        <div>
-            Recipe Card
-        </div>
+        <s.CardContainer>
+            <s.Image imageUrl={recipe.strMealThumb} />
+            <s.Title>{recipe.strMeal}</s.Title>
+            <s.Text>{recipe.strArea}</s.Text>
+            <s.Text>{recipe.strTags}</s.Text>
+        </s.CardContainer>
     )
 }
 
